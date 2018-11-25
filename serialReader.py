f23d80090a11937d5e8efc34244365960d628097
cmput274 <cmput274@ualberta.ca>
2018-11-25 00:03:42 -0700

    clean up workspace

--- serialReader.py
+++ serialReader.py
@@ -1,5 +1,17 @@
 import serial
 ser = serial.Serial("/dev/ttyACM0", 19200)
+apins = {}
+dpins = {}
 while True:
-    cc = str(ser.readline())
-    print(cc[2:][:-5])
+    line = str(ser.readline())[2:][:-5]
+    print(line)
+    line = line.split(":")
+    pin = line[0]
+    value = line[1]
+    if pin[0] == 'A':
+        pin = pin[1:]
+        apins[pin] = int(value)
+    else:
+        dpins[pin] = int(value)
+    print(apins)
+    print(dpins)
