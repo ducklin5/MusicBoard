c55271b3c2dc616131f35a2972240900d5d763b8
cmput274 <cmput274@ualberta.ca>
2018-11-25 14:52:30 -0700

    fixed merge conflict

--- serialReader.py
+++ serialReader.py
@@ -1,10 +1,6 @@
 import serial
 ser = serial.Serial("/dev/ttyACM0", 19200)
 
-while True:
-    cc = str(ser.readline())
-    print(cc[2:][:-5])
-
 pins = {}
 while True:
     line = str(ser.readline())[2:][:-5]
--- waveform.py
+++ waveform.py
@@ -19,21 +19,21 @@ audio = audio.astype(np.int16)
 
 # zoom = round(sample_rate/A_freq)
 
-# bytestream = audio.tobytes()
-# pya = pyaudio.PyAudio()
-# stream = pya.open(format=pya.get_format_from_width(width=2), channels=1, rate=sample_rate, output=True)
-# while True:
-#     stream.write(bytestream)
-# stream.stop_stream()
-# stream.close()
-#
-# pya.terminate()
+bytestream = audio.tobytes()
+pya = pyaudio.PyAudio()
+stream = pya.open(format=pya.get_format_from_width(width=2), channels=1, rate=sample_rate, output=True)
+while True:
+    stream.write(bytestream)
+stream.stop_stream()
+stream.close()
+
+pya.terminate()
 
 
 plt.plot(t, audio, 'x')
 plt.show()
 
-wave = sa.WaveObject(audio, 1, 2, sample_rate)
-while True:
-    playObj = wave.play()
-    playObj.wait_done()
+# wave = sa.WaveObject(audio, 1, 2, sample_rate)
+# while True:
+#     playObj = wave.play()
+#     playObj.wait_done()
