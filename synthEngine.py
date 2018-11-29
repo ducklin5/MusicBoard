dc1945d32929e363e5a92f360b4c1d13c9ce1dca
cmput274 <cmput274@ualberta.ca>
2018-11-29 00:01:02 -0700

    XD sweet mother of god, that engine is good..... still has bugs though

--- synthEngine.py
+++ synthEngine.py
@@ -3,7 +3,7 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import time
 import pygame
-from enum import IntEnum, Enum
+from enum import Enum
 from threading import Thread
 
 sample_rate = 44100
@@ -16,20 +16,21 @@ pygame.mixer.init()
 pygame.mixer.set_num_channels(100)
 
 
+# https://en.wikipedia.org/wiki/MIDI_tuning_standard
+def midi(midiKey):
+    # midi key 69 --> A4
+    # midi key 70 --> A#4
+    # midi key 71 --> B4
+    freq = (440) * (2**((midiKey-69)/12))
+    return freq
+
+
 # https://docs.python.org/3/library/enum.html
-class Note(IntEnum):
-    A = 440
-    B = 493.88
-    C = 523.25
-    D = 440
-    E = 440
-    F = 440
-
-
 class Wave(Enum):
     SAW = 0
     SINE = 1
     SQUARE = 2
+    TRIANGLE = 3
 
 
 def playArray(array, repeat=False):
@@ -106,7 +107,8 @@ class oscillator:
         waveforms = {
             Wave.SINE: np.sin(theta),
             Wave.SAW: signal.sawtooth(theta, 0),
-            Wave.SQUARE: signal.square(theta)
+            Wave.SQUARE: signal.square(theta),
+            Wave.TRIANGLE: signal.sawtooth(theta, 0.5)
         }
         return self.scale * waveforms.get(self.form)
 
@@ -123,7 +125,7 @@ class oscillator:
 
 class Envelope:
     def __init__(self):
-        self.Adur = 0.5
+        self.Adur = 0.05
         self.Dval = 0.5
         self.Ddur = 0.2
         self.Sval = 1
@@ -132,7 +134,10 @@ class Envelope:
         self.sustains = {}
 
     def start(self, sound):
-        Thread(target=self.__start__, args=(sound,)).run()
+        # http://sebastiandahlgren.se/2014/06/27/running-a-method-as-a-background-thread-in-python/
+        thread = Thread(target=self.__start__, args=(sound,))
+        thread.daemon = True
+        thread.start()
 
     def __start__(self, sound):
         sound.set_volume(0)
@@ -151,41 +156,61 @@ class Envelope:
                     self.Dval + (self.Sval-self.Dval)*elapsed/self.Ddur)
             elapsed = time.time() - start
 
+        sound.set_volume(self.Sval)
+
     def release(self, sound):
-        Thread(target=self.__release__, args=(sound,)).run()
+        thread = Thread(target=self.__release__, args=(sound,))
+        thread.daemon = True
+        thread.start()
 
     def __release__(self, sound):
         start = time.time()
         elapsed = 0
         while elapsed < self.Rdur:
             time.sleep(1/sample_rate)
-            sound.set_volume(self.Sval-self.Sval*elapsed/self.Adur)
+            sound.set_volume(self.Sval-self.Sval*elapsed/self.Rdur)
             elapsed = time.time() - start
         sound.stop()
 
 
 if __name__ == "__main__":
+    # create a synth
     mySynth = synth()
+
+    # with 3 oscillators
     mySynth.sources[0].form = Wave.SQUARE
-    mySynth.sources[0].scale = 0.25
-    mySynth.draw(Note.A)
+    mySynth.sources[1].form = Wave.TRIANGLE
+    mySynth.sources.append(oscillator())
+    mySynth.sources[2].form = Wave.SINE
 
-    myEnv = Envelope()
+    # set the oscillator volumes
+    mySynth.sources[0].scale = 0.5
+    mySynth.sources[1].scale = 0.75
+    mySynth.sources[2].scale = 0.75
+
+    # draw this output pf the synth (and its oscillators)
+    # for freq = midi(69)
+    mySynth.draw(midi(69))
+
+    #
+    mySynth.adsr.Adur = 0.3
+    mySynth.adsr.Ddur = 0.1
+    mySynth.adsr.Dval = 1
+    mySynth.adsr.Sval = 0.1
+    mySynth.adsr.Rdur = 0.2
+
+    def myK(k):
+        mySynth.play(midi(k))
+        time.sleep(0.4)
+        mySynth.release(midi(k))
+        time.sleep(0.1)
+
     while True:
-        mySynth.play(Note.B)
-        time.sleep(3)
-        mySynth.release(Note.B)
-        time.sleep(3)
-
-    for i in range(3):
-
-        mySynth.sources[0].play(Note.A, 2)
-        time.sleep(3)
-        mySynth.sources[1].play(Note.A, 2)
-        time.sleep(3)
-        print("Play Now!")
-        mySynth.play(Note.A)
-        time.sleep(3)
-        print("Release Now!")
-        mySynth.release(Note.A)
-        time.sleep(3)
+        myK(69)
+        myK(71)
+        myK(72)
+        myK(74)
+        myK(69)
+        myK(71)
+        myK(72)
+        myK(77)
