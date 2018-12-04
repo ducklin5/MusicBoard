4b08ad983a7d6b7d4f235b104a4a62f4c8bc1f7f
cmput274 <cmput274@ualberta.ca>
2018-12-04 06:08:04 -0700

    Finally pulled properly

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import pygame
 import serialReader
 import synthEngine as se
 from threading import Thread
- 
+
 # inputs to be used
 inputs = {"2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0,
           "11": 0, "12": 0, "13": 0, "22": 0, "23": 0, "25": 0, "27": 0, "29": 0, "31": 0}
@@ -12,27 +12,6 @@ class SynthUI():
 
     def __init__(self):
         self.synth = se.Synth()
-# <<<<<<< HEAD
- 
-#     def set_wave(self):
-#         pass
- 
-#     def set_vol(self):
-#         pass
- 
-#     def drawWaves(self):
-#         self.synth.draw()
- 
-#     def drawEnvelope(self):
-#         self.synth.adsr.draw()
- 
-#     def drawFilter(self):
-#         self
- 
-#     def drawUI(self):
-#         pass
- 
-# =======
         self.synthPlot = None
         self.lfoPlot = None
         self.filterPlot = None
@@ -88,13 +67,11 @@ def run():
     global inputs
     # import your script B
     pygame.init()
- 
+
     myFont = pygame.font.SysFont("Times New Roman", 18)
- 
-    display_width=947
-    display_height=609
- 
-    points = 0
+
+    display_width = 947
+    display_height = 609
 
     black = (0, 0, 0)
     white = (255, 255, 255)
@@ -102,9 +79,9 @@ def run():
     green = (0, 255, 0)
     blue = (0, 0, 255)
     gray = (128, 128, 128)
- 
+
     gameDisplay = pygame.display.set_mode((display_width, display_height))
- 
+
     pygame.display.set_caption('Music Board')
     clock = pygame.time.Clock()
     # print(pygame.image.get_extended())
@@ -113,7 +90,7 @@ def run():
     Button1Pressed = pygame.image.load('images/Button1Pressed.png')
     Knob1 = pygame.image.load('images/Knob1.png')
     quit = False
- 
+
     xKnob1 = 500
     yKnob1 = 250
     xButton1 = 1
@@ -122,55 +99,12 @@ def run():
     yBoxBackground = 1
 
     # initialize synth fonts
-
     # i = 0
     # synthFonts=[]
     # while i < 20:
     #     synthFonts[i] = myFont.render('Synth {}'.format(i),
     #     i+=1
     #myFont.render('Synth {}').format(i)
- 
-    # Synth1 = se.Synth(2)
-    # Synth1.sources[0].form = se.Wave.SINE
-    # Synth1.sources[1].form = se.Wave.SQUARE
-   
- 
-    ###### HERE
-    ui = SynthUI()
-    
-    ####
-   
-    Piano = [0] * 12 # Initializes piano keys
-    while not quit:
- 
-        Piano[0] = (inputs["2"]) #C
-        Piano[1] = (inputs["3"]) #C sharp
-        Piano[2] = (inputs["4"]) #D
-        Piano[3] = (inputs["5"]) #D sharp
-        Piano[4] = (inputs["6"]) #E
-        Piano[5] = (inputs["7"]) #F
-        Piano[6] = (inputs["8"]) #F sharp
-        Piano[7] = (inputs["9"]) #G
-        Piano[8] = (inputs["10"]) #G sharp
-        Piano[9] = (inputs["11"]) #A
-        Piano[10] = (inputs["12"]) #A sharp
-        Piano[11] = (inputs["13"]) #B
- 
-        LeftButton = (inputs["23"])
-        RightButton = (inputs["22"])
-
-        # Volume = int(inputs["A0"])
-        # HJoystick = (inputs["A14"])
-        # VJoystick = (inputs["A15"])
-        # ClickJoystick = (inputs["25"])
- 
-        gameDisplay.blit(BoxBackground,(xBoxBackground,yBoxBackground))
-        #gameDisplay.blit(ui.drawWaves,(1,1))
-        i=0
-        while i < len(Piano):
-            if Piano[i]:
-                se.myK(Synth1,i+72)
-            i+=1
 
     synth1UI = SynthUI()
 
@@ -184,32 +118,21 @@ def run():
         ####
         ###########
 
-            #gameDisplay.blit(Button1,(xButton1,yButton1))
- 
-       
-        # gameDisplay.blit(,,)
-        # gameDisplay.blit(,,)
-        # gameDisplay.blit(,,)
-        # gameDisplay.blit(,,)
-        # gameDisplay.blit(,,)
-        # gameDisplay.blit(,,)
-        # gameDisplay.blit(,,)
+        # if PianoA:
+        #     #gameDisplay.blit(Button1Pressed,(xButton1,yButton1))
+        #     se.myK(Synth1,69)
 
-        # prevInputs = inputs
+        # if PianoAS
+        # else:
         # gameDisplay.blit(Button1,(xButton1,yButton1))
         synth1UI.drawUI(gameDisplay)
         synth1UI.playKeys()
 
         pygame.display.update()
         clock.tick(30)
- 
+
     pygame.quit
     quit()
- 
- 
-# Thread(target=serialReader.run, args=("/dev/ttyACM0", inputs)).start()
-# Thread(target=run).start()
-# =======
 
 
 Thread(target=serialReader.run, args=("/dev/ttyACM0", inputs, True)).start()
